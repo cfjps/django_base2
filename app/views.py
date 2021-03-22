55541a846eefcf9f21e061b317b421b66315858a
unknown <jpscriven@live.co.za>
2021-03-22 10:34:27 +0200

    update to pull onto EC2 website2

--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Development Server - Website # 1 - Updated with GIT</h1>')
+    return HttpResponse('<h1>Development Server - Website # 2 - Updated with GIT</h1>')
